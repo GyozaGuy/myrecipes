190a03e80bc041ed6a03c22506e971f318d23bf1
Steven Stromberg <gyozaguy@gmail.com>
2017-07-31 09:02:57 -0600

    Add recipe creation with tests

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -31,4 +31,28 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @recipe1.description, response.body
     assert_match @chef.chefname, response.body
   end
+
+  test 'create new valid recipe' do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    recipe_name = 'chicken saute'
+    recipe_description = 'add chicken, add vegetables, cook for 20 minutes, service delicious meal'
+    assert_difference 'Recipe.count', 1 do
+      post recipes_path, params: { recipe: { name: recipe_name, description: recipe_description } }
+    end
+    follow_redirect!
+    assert_match recipe_name.capitalize, response.body
+    assert_match recipe_description, response.body
+  end
+
+  test 'reject invalid recipe submission' do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: { name: '', description: '' } }
+    end
+    assert_template 'recipes/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
 end
